bad1fd64f84afea2804467f150c1962cf8e918ef
Markus Daniel <markusdaniel34@gmail.com>
2021-03-05 02:01:47 +0700

    dev-read-api

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -1,6 +1,7 @@
 const {User} = require('../models')
-const Bcrypt = require('../helper/bcryptjs')
-const Jwt = require('../helper/JasonWebToken')
+const Bcrypt = require('../helpers/bcryptjs')
+const Jwt = require('../helpers/JasonWebToken')
+const { Country } = require('../helpers/api')
 
 class Controller {
     static loginPost(req,res){
@@ -21,18 +22,28 @@ class Controller {
             }
         })
         .catch((err)=>{
-            res.send(err)
+            res.status(400)
         })
     }
-    static registerPost(req,res,next){
+    static registerPost(req,res){
         let input = {
             email : req.body.email,
             password : req.body.password,
             name : req.body.name
         }
+
         User.create(input)
         .then((data)=>{
             res.status(201).json(data)
+        })
+        .catch((err)=>{
+            res.send(err)
+        })
+    }
+    static test(req,res){
+        Country()
+        .then((data)=>{
+            res.send(data)
         })
         .catch((err)=>{
             res.send(err)
