a481bb972f7f1b47b605d9f88ccbca056e87e8b1
Markus Daniel <markusdaniel34@gmail.com>
2021-03-05 14:15:32 +0700

    dev-authentication

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,14 @@
 const router = require("express").Router();
 const Controller = require('../Controllers/userController');
+const auth = require('../Middleware/auth')
 
+router.post('/register',Controller.registerPost)
 router.post('/login',Controller.loginPost)
 router.post('/loginGoogle',Controller.loginGoogle)
-router.post('/register',Controller.registerPost)
+router.use(auth)
 router.get('/news',Controller.GetCovidNews)
 router.get('/data',Controller.GetDataCovid)
 router.get('/travel',Controller.GetDataTravelCovid)
 
 module.exports = router;
+ 
